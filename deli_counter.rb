4e6ba6655bc56b097fbdd3e68079125a20e1bc8a
Nathan Bohn <nathantbohn@gmail.com>
2020-02-04 22:51:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,9 @@
 # Write your code here.
 def line(array)
-    puts "The line is currently empty."
-
+    if array.size>0
+       puts "The line is currently empty."
+      
+  
 end 
 =begin
 def take_a_number(array, person)
